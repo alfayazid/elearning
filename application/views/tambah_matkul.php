f3bf7d6f7601cc447027163c2a8f1fc4c1633261
Januar Fonti <januarfonti@yahoo.com>
2014-06-08 01:26:06 +0700

    form tambah matkul

---
+++ application/views/tambah_matkul.php
@@ -0,0 +1,40 @@
+<br />
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <? echo $judul ?>
+    </div>
+        <div class="panel-body">
+            
+            <?
+              $attr = array(
+                'role' => 'form',
+                'class' => 'form-horizontal'
+              ); 
+             ?>
+            <?php echo form_open('rest_client/proses_tambah_matkul',$attr) ?>
+                <div class="form-group">
+                    <label for="inputEmail3" class="col-sm-2 control-label">Nama Mata Kuliah</label>
+                    <div class="col-sm-10">
+                        <input name="nama_matkul"  type="text" class="form-control">
+                    </div>
+                </div>
+
+  
+                  <div class="form-group">
+                    <label for="inputPassword3" class="col-sm-2 control-label">Enrollment Key</label>
+                    <div class="col-sm-10">
+                        <input name="enroll" type="text" class="form-control" id="inputPassword3">
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="inputPassword3" class="col-sm-2 control-label"></label>
+                    <div class="col-sm-10">
+                        <button class="btn btn-danger" type="submit">Submit</button>
+                    </div>
+                  </div>
+
+
+            </form>
+        </div>
+</div>
