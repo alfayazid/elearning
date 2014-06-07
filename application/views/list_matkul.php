e5a679c90e4b903189e775faa36404f7477d4b8c
Januar Fonti <januarfonti@yahoo.com>
2014-06-08 01:25:30 +0700

    list_matkul

--- application/views/list_matkul.php
+++ application/views/list_matkul.php
@@ -1,5 +1,5 @@
     <h1 class="page-header"><span class="glyphicon glyphicon-th-list"></span>  Daftar Mata Kuliah</h1>
-    <a href="<? echo base_url('index.php/rest_client/matkul_baru');?>" class="btn btn-danger"><span class="glyphicon glyphicon-plus"></span>  Tambah Mata Kuliah</a>
+    <a href="<? echo base_url('index.php/rest_client/tambah_matkul');?>" class="btn btn-danger"><span class="glyphicon glyphicon-plus"></span>  Tambah Mata Kuliah</a>
     <br />
     <? if(isset($matkul)){
         foreach ($matkul as $row ) { 
